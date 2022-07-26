2497aa8739032f194f98aab595b957cfe90f87d2
kjh <whdgns9612@gmail.com>
2022-07-26 16:16:42 +0900

    템플릿 패턴 수강완료

--- src/main/java/hello/advanced/config/LogTraceConfig.java
+++ src/main/java/hello/advanced/config/LogTraceConfig.java
@@ -1,6 +1,5 @@
-package hello.advanced;
+package hello.advanced.config;
 
-import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
